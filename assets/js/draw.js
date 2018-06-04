2309d0a03ef71160b6be9c00266896073ee6604f
JonSteinn <jonsteinn@gmail.com>
2018-06-04 09:02:33 +0000

    oob bug fix, oval+circle, undo

--- assets/js/draw.js
+++ assets/js/draw.js
@@ -1,23 +1,39 @@
 (function () {
 
+    // The object holding what to draw
     let drawer = {
         shapes: [],
-        selectedShape: 'rectangle',
+        selectedShape: 'circle',
         canvas: document.getElementById('canvas'),
         ctx: document.getElementById('canvas').getContext('2d'),
         selectedElement: null,
         availableShapes: {
-            RECTANGLE: 'rectangle'
+            RECTANGLE: 'rectangle',
+            OVAL: 'oval',
+            CIRCLE: 'circle'
         }
     };
 
-    function drawCanvas() {
+    // Object to simulate unbuffered keyboard io for the keys we need
+    let keyDown = {
+        ctrl: false,
+        shift: false
+    };
+
+    /**
+     * Wipes the canvas and redraws everything
+     */
+    function redraw() {
+        drawer.ctx.clearRect(0, 0, drawer.ctx.canvas.width, drawer.ctx.canvas.height);
+
         if (drawer.selectedElement) {
             drawer.selectedElement.render(drawer.ctx);
         }
 
         for (let i = 0; i < drawer.shapes.length; i++) {
-            drawer.shapes[i].render(drawer.ctx);
+            if (drawer.shapes[i]) {
+                drawer.shapes[i].render(drawer.ctx);
+            }
         }
     }
 
@@ -26,22 +42,58 @@
             case drawer.availableShapes.RECTANGLE:
                 drawer.selectedElement = new Rectangle({x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
                 break;
+            case drawer.availableShapes.OVAL:
+                drawer.selectedElement = new Oval({x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0, 0);
+                break;
+            case drawer.availableShapes.CIRCLE:
+                drawer.selectedElement = new Circle({x: mouseEvent.offsetX, y: mouseEvent.offsetY}, 0);
+                break;
 
         }
     });
-
 
     drawer.canvas.addEventListener('mousemove', function (mouseEvent) {
         if (drawer.selectedElement) {
-            drawer.ctx.clearRect(0, 0, drawer.ctx.canvas.width, drawer.ctx.canvas.height);
             drawer.selectedElement.resize(mouseEvent.offsetX, mouseEvent.offsetY);
-            drawCanvas();
+            redraw();
         }
     });
 
-    drawer.canvas.addEventListener('mouseup', function () {
-        drawer.shapes.push(drawer.selectedElement);
-        drawer.selectedElement = null;
+    document.addEventListener('mouseup', function (mouseEvent) {
+        if (drawer.selectedElement) {
+            drawer.shapes.push(drawer.selectedElement);
+            drawer.selectedElement = null;
+        }
+    });
+
+    document.addEventListener('keypress', function (evt) {
+
+        if (evt.ctrlKey) {
+            keyDown.ctrl = true;
+        }
+        if (evt.shiftKey) {
+            keyDown.shift = true;
+        }
+
+        if (evt.key === 'z' && keyDown.ctrl) {
+            if (keyDown.shift) {
+                // TODO: redo
+            } else {
+                if (drawer.shapes.length > 0) {
+                    drawer.shapes.pop();
+                    redraw();
+                }
+            }
+        }
+    });
+
+    document.addEventListener('keyup', function (evt) {
+        if (evt.ctrlKey) {
+            keyDown.ctrl = false;
+        }
+        if (evt.shiftKey) {
+            keyDown.shift = false;
+        }
     });
 
 })();
--- assets/js/shapes.js
+++ assets/js/shapes.js
@@ -1,37 +1,116 @@
-
-
+/**
+ *
+ */
 class Shape {
+    /**
+     *
+     * @param position
+     */
     constructor(position) {
         this.position = position;
     }
 
+    /**
+     *
+     * @param ctx
+     */
     render(ctx) {
 
     }
 
+    /**
+     *
+     * @param position
+     */
     move(position) {
         this.position = position;
     }
 
+    /**
+     *
+     * @param x
+     * @param y
+     */
     resize(x, y) {
 
     }
 }
 
-
+/**
+ *
+ */
 class Rectangle extends Shape {
+    /**
+     *
+     * @param position
+     * @param width
+     * @param height
+     */
     constructor(position, width, height) {
         super(position);
         this.width = width;
         this.height = height;
     }
 
+    /** @inheritdoc */
     render(ctx) {
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
 
+    /** @inheritdoc */
     resize(x, y) {
         this.width = x - this.position.x;
         this.height = y - this.position.y;
     }
 }
+
+/**
+ *
+ */
+class Oval extends Shape {
+    /**
+     *
+     * @param position
+     * @param xRadius
+     * @param yRadius
+     */
+    constructor(position, xRadius, yRadius) {
+        super(position);
+        this.xRadius = xRadius;
+        this.yRadius = yRadius;
+    }
+
+    /** @inheritdoc */
+    render(ctx) {
+        ctx.beginPath();
+        ctx.ellipse(this.position.x, this.position.y, this.xRadius, this.yRadius, 0, 0, 2*Math.PI);
+        ctx.fill();
+    }
+
+    /** @inheritdoc */
+    resize(x, y) {
+        this.xRadius = Math.abs(x - this.position.x);
+        this.yRadius = Math.abs(y - this.position.y);
+    }
+}
+
+/**
+ *
+ */
+class Circle extends Oval {
+    /**
+     *
+     * @param position
+     * @param radius
+     */
+    constructor(position, radius) {
+        super(position, radius, radius);
+    }
+
+    /** @inheritdoc */
+    resize(x, y) {
+        this.xRadius = Math.max(Math.abs(x - this.position.x), Math.abs(y - this.position.y));
+        this.yRadius = this.xRadius;
+    }
+}
+
